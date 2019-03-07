35da7ba10b2a08d7dedd44d44ea2c5445e2f596e
Donna Amos <donnacamos.developer@gmail.com>
2019-03-07 04:52:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 # Write your code here.
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz.deli "
+  puts "Welcome, #{name}. You are number #{katz.deli.length} "
 end 
